0ad5e1ebd100285b5943178c23b16566880abee9
Edison Lien <fvckedison@gmail.com>
2022-01-23 02:41:51 +0800

    test

--- index.js
+++ index.js
@@ -1,10 +1,39 @@
-var linebot = require('linebot');
-// 用於辨識Line Channel的資訊
-var bot = linebot({ channelId: '1656828545', channelSecret: 'd3729fab1a39345828dd300e95f2f2d4', channelAccessToken: '6AcSOYuoJpyOoD7W3c5DxMTR8qmA69lveDOiJlvqghgSrO3X4uz7y'
+const line = require('@line/bot-sdk');
+var express = require('express');
+const config = {
+  channelAccessToken: '6AcSOYuoJpyOoD7W3c5DxMTR8qmA69lveDOiJlvqghgSrO3X4uz7y/+h+YpFkWbcJt/gg2LDzG9tJtBwlZow6Inyc+nIDT1768GdV7MAXkMeOLdPLw3DdPuJic3uU18BZPYvt6wAjyt7btFOl61QygdB04t89/1O/w1cDnyilFU=',
+  channelSecret: 'd3729fab1a39345828dd300e95f2f2d4'
+};
+// create LINE SDK client
+const client = new line.Client(config);
+// create Express app
+// about Express itself: <https://expressjs.com/>
+const app = express();
+// register a webhook handler with middleware
+// about the middleware, please refer to docgit 
+app.post('/callback', line.middleware(config), (req, res) => {
+	console.log(req, res)
+  Promise
+    .all(req.body.events.map(handleEvent))
+    .then((result) => res.json(result))
+    .catch((err) => {
+      console.error(err);
+      res.status(500).end();
+    });
 });
-// 當有人傳送訊息給Bot時
-bot.on('message', function (event) { // event.message.text是使用者傳給bot的訊息 // 使用event.reply(要回傳的訊息)方法可將訊息回傳給使用者 event.reply(event.message.text).then(function (data) { // 當訊息成功回傳後的處理 }).catch(function (error) { // 當訊息回傳失敗後的處理 });
-});
-// Bot所監聽的webhook路徑與port
-bot.listen('/linewebhook', 3000, function () { console.log('[BOT已準備就緒]');
+// event handler
+function handleEvent(event) {
+  if (event.type !== 'message' || event.message.type !== 'text') {
+    // ignore non-text-message event
+    return Promise.resolve(null);
+  }
+  // create a echoing text message
+  const echo = { type: 'text', text: event.message.text };
+  // use reply API
+  return client.replyMessage(event.replyToken, echo);
+}
+// listen on port
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`listening on ${port}`);
 });
