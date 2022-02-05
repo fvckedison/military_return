bbb3062311b9669a7e7b44ee59b432d566c3fe5d
Edison Lien <fvckedison@gmail.com>
2022-02-05 13:25:14 +0800

    add lowdb

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
 const line = require('@line/bot-sdk');
 var express = require('express');
+const low = require('lowdb')
+const FileSync = require('lowdb/adapters/FileSync')
+const adapter = new FileSync('db.json')
+const db = low(adapter)
+
+db.defaults({ returnMessage: [] })
+  .write();
 const config = {
   channelAccessToken: '6AcSOYuoJpyOoD7W3c5DxMTR8qmA69lveDOiJlvqghgSrO3X4uz7y/+h+YpFkWbcJt/gg2LDzG9tJtBwlZow6Inyc+nIDT1768GdV7MAXkMeOLdPLw3DdPuJic3uU18BZPYvt6wAjyt7btFOl61QygdB04t89/1O/w1cDnyilFU=',
   channelSecret: 'd3729fab1a39345828dd300e95f2f2d4'
 };
-// create LINE SDK client
 const client = new line.Client(config);
 
-// create Express app
-// about Express itself: https://expressjs.com/
 const app = express();
 
-// register a webhook handler with middleware
-// about the middleware, please refer to doc
 app.post('/callback', line.middleware(config), (req, res) => {
   Promise
     .all(req.body.events.map(handleEvent))
@@ -22,46 +24,61 @@ app.post('/callback', line.middleware(config), (req, res) => {
       res.status(500).end();
     });
 });
-let dataArray=['','','','','','','','','','','','','','','']
 // event handler
 function handleEvent(event) {
-  let echo=null
+  let echo = null
   if (event.type !== 'message' || event.message.type !== 'text') {
     // ignore non-text-message event
     return Promise.resolve(null);
   }
-  else{
-    
-    if(event.message.text=='開始回報'){
-      dataArray=['','','','','','','','','','','','','','','']
-      echo = { type: 'text', text: '請各位弟兄現在開始回報\n請複製以下格式\n\n120001 陳威助\n在哪：家\n做什麼：玩手機\n有無飲酒：無\n2200後是否在家不出門：是\n' }
+  else {
+    if (event.message.text == '開始回報') {
+      echo = { type: 'text', text: '請各位弟兄現在開始回報\n請複製以下格式\n\n12001 陳威助\n在哪：家\n做什麼：玩手機\n有無飲酒：無\n2200後是否在家不出門：是\n' }
       return client.replyMessage(event.replyToken, echo);
-    }else if(event.message.text=='查詢詳情'){
-      console.log(dataArray)
-      let unSignList=''
-      let signList=''
-      for(let i =0;i<dataArray.length;i++){
-        if(dataArray[i]==''){
-          unSignList+=`120${i+31}未回報\n\n`
-        }else{
-          signList+=`${dataArray[i]}\n\n`
-        }
+    } else if (event.message.text == '查詢詳情') {
+      let result = db.get('returnMessage').orderBy('id:', 'asc').value()
+      let signNumber = ''
+      let signList = ''
+      result.forEach(element => {
+        signList += `${element.message}\n\n`;
+        signNumber += `${element.id} `
+      });
+      if(db.get('returnMessage').value().length!=0){
+        echo = { type: 'text', text: `${signList}已完成回報名單:\n${signNumber}` }
+        return client.replyMessage(event.replyToken, echo);
+      }else{
+        echo = { type: 'text', text: '尚未有人回報' }
+        return client.replyMessage(event.replyToken, echo);
       }
-      echo = { type: 'text', text: signList+unSignList }
-      return client.replyMessage(event.replyToken, echo);
-      //  echo = { type: 'text', text: dataArray }
-      //  return client.replyMessage(event.replyToken, echo);
-    }else if (parseInt(event.message.text.substr(3,2),10)>30 &&parseInt(event.message.text.substr(3,2),10)<46){
-      let id=parseInt(event.message.text.substr(3,2),10)
-      dataArray[id-31]=event.message.text
+      
+      
+    } else if (parseInt(event.message.text.substr(0, 5), 10) != NaN && parseInt(event.message.text.substr(0, 5), 10) > 10000&& parseInt(event.message.text.substr(0, 5), 10) < 99999) {
+      let id = parseInt(event.message.text.substr(0, 5), 10)
+      let result = db.get('returnMessage')
+        .find({ id: id })
+        .value()
+      if (result != undefined) {
+        db.get('users')
+          .find({ id: id })
+          .assign({ message: event.message.text })
+          .write();
+      } else {
+        db.get('returnMessage')
+          .push({ message: event.message.text })
+          .last()
+          .assign({ id: id })
+          .write();
+      }
+    }
+    else if (event.message.text.substr(0, 2) == '刪除') {
+      let id =parseInt(event.message.text.substr(2, 5), 10)
+      db.get('returnMessage')
+        .remove({ id: id })
+        .write();
     }
   }
-  // create a echoing text message
-  // use reply API
-  
 }
 
-// listen on port
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`listening on ${port}`);
